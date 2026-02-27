b63941b2cbd57ac69a782fb4a867fde5c6f395f0
PaperMtn <andrew.byford11+github@gmail.com>
2026-02-27 21:53:15 +0000

    feat: implement CLI commands for marketplace integrations

--- src/secops/cli/cli_client.py
+++ src/secops/cli/cli_client.py
@@ -39,6 +39,9 @@ from secops.cli.commands.stats import setup_stats_command
 from secops.cli.commands.udm_search import setup_udm_search_view_command
 from secops.cli.commands.watchlist import setup_watchlist_command
 from secops.cli.commands.rule_retrohunt import setup_rule_retrohunt_command
+from secops.cli.commands.marketplace_integrations import (
+    setup_marketplace_integrations_command,
+)
 from secops.cli.utils.common_args import add_chronicle_args, add_common_args
 from secops.cli.utils.config_utils import load_config
 from secops.exceptions import AuthenticationError, SecOpsError
@@ -189,6 +192,7 @@ def build_parser() -> argparse.ArgumentParser:
     setup_dashboard_query_command(subparsers)
     setup_watchlist_command(subparsers)
     setup_rule_retrohunt_command(subparsers)
+    setup_marketplace_integrations_command(subparsers)
 
     return parser
 
